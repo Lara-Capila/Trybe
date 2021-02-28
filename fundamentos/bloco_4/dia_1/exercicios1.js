0fb0f9d680158662efb7cf7af802fd82117e7d43
Lara Capila <laracapilak@gmail.com>
2021-02-28 18:07:00 -0300

    exercício número 1

---
+++ fundamentos/bloco_4/dia_1/exercicios1.js
@@ -0,0 +1,9 @@
+// 1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados.
+let a = 8;
+let b = 28;
+
+console.log(a + b);
+console.log(b - a);
+console.log(a * b);
+console.log(b / a);
+console.log(b % a);
