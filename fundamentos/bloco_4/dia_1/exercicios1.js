dc808d2d90aa79dd60c0f28ca66b0149733fb80c
Lara Capila <laracapilak@gmail.com>
2021-02-28 18:11:46 -0300

    exercício número 2

--- fundamentos/bloco_4/dia_1/exercicios1.js
+++ fundamentos/bloco_4/dia_1/exercicios1.js
@@ -7,3 +7,14 @@ console.log(b - a);
 console.log(a * b);
 console.log(b / a);
 console.log(b % a);
+
+// 2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let number1 = 25;
+let number2 = 40;
+
+if (number1 > number2) {
+    console.log("Number1 maior que number2")
+} else {
+    console.log("Number2 maior que number1")
+}
