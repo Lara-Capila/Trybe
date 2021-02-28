cb839979e0141c1c873c1f1bb2df38ac7aec3eab
Lara Capila <laracapilak@gmail.com>
2021-02-28 20:26:08 -0300

    exercício número 7

--- fundamentos/bloco_4/dia_1/exercicios1.js
+++ fundamentos/bloco_4/dia_1/exercicios1.js
@@ -93,3 +93,37 @@ switch (peca.toLowerCase()) {
     default:
         console.log("Peça inválida");
 }
+
+// 7.Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+
+let nota = -0;
+
+switch (true) {
+    case (nota >= 90):
+        console.log("A");
+        break;
+
+    case (nota >= 80):
+        console.log("B");
+        break;
+
+    case (nota >= 70):
+        console.log("C");
+        break;
+
+    case (nota >= 60):
+        console.log("D");
+        break;
+
+    case (nota >= 50):
+        console.log("E");
+        break;
+
+    case (nota < 50):
+        console.log("A");
+        break;
+
+    case (nota < 0 || nota > 100):
+        console.log("Erro: Nota inválida");
+        break;
+}
