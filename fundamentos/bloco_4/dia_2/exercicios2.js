00f353d01a37e7a8a90f4435e67c44fdcbcc9ad4
Lara Capila <laracapilak@gmail.com>
2021-03-01 15:55:57 -0300

    exercicio número 2

--- fundamentos/bloco_4/dia_2/exercicios2.js
+++ fundamentos/bloco_4/dia_2/exercicios2.js
@@ -1,13 +1,17 @@
-// let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
-
-// for (let index = 0; index < groceryList.length; index++) {
-//     console.log(groceryList[index])
-// }
+// 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
-// 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
 
 for (let valores = 0; valores < numbers.length; valores++) {
     console.log(numbers[valores]);
 }
+
+//2. Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let valores = 0; valores < numbers.length; valores++) {
+    soma += numbers[valores];
+}
+
+console.log(soma);
