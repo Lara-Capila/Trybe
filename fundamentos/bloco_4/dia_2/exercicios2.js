5fabe98845a972218b2b08fa0357447efab1b0d7
Lara Capila <laracapilak@gmail.com>
2021-03-01 15:41:05 -0300

    exercico número 1

---
+++ fundamentos/bloco_4/dia_2/exercicios2.js
@@ -0,0 +1,13 @@
+// let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+
+// for (let index = 0; index < groceryList.length; index++) {
+//     console.log(groceryList[index])
+// }
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+for (let valores = 0; valores < numbers.length; valores++) {
+    console.log(numbers[valores]);
+}
