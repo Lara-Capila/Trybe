57f09ac8c4215ef31b224c5bd0e5cc0169b1f48e
Lara Capila <laracapilak@gmail.com>
2021-03-03 09:22:58 -0300

    exercicio 3 e 4

--- fundamentos/bloco_4/dia_3/asteriscos.js
+++ fundamentos/bloco_4/dia_3/asteriscos.js
@@ -1,4 +1,4 @@
-// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . 
 
 let n = 5;
 let asterisco = '*';
@@ -12,7 +12,7 @@ for (let i = 0; i <= n; i +=1) {
   console.log (espaco);
 };
 
-// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. 
 
 
 let quantAste = 5;
@@ -24,3 +24,47 @@ for (let index = 0; index <= quantAste; index += 1) {
   espaco = espaco + aste;
 };
 
+// 3- Agora inverta o lado do triângulo. 
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n;
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1;
+};
+
+// 4- Depois, faça uma pirâmide com n asteriscos de base:
+
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
+
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex > controlLeft && columnIndex < controlRight) {
+      inputLine = inputLine + symbol;
+    } else {
+      inputLine = inputLine + ' ';
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  controlRight += 1;
+  controlLeft -= 1
+};
