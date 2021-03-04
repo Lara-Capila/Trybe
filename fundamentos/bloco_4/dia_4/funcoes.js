4f0c089b20851a7c25e23fd40dce728f4ec31c3e
Lara Capila <laracapilak@gmail.com>
2021-03-03 22:42:45 -0300

    Finalização do exercicio numero um e inicio do numero dois

--- fundamentos/bloco_4/dia_4/funcoes.js
+++ fundamentos/bloco_4/dia_4/funcoes.js
@@ -1,6 +1,5 @@
 // 1. Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
 
-
 function verificaPalindromo (palavra){
     let word = "";
     for (let i = palavra.length -1; i >= 0; i--){
@@ -12,4 +11,18 @@ function verificaPalindromo (palavra){
         return false;
     }
 }
-console.log(verificaPalindromo("arara"));
+
+console.log(verificaPalindromo("desenvolvimento"));
+
+
+// 2. Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function verificaIndice(array){
+    let n = [];
+    for(let i = 0; array <= i; i++){
+       n = Math.max.apply([null, array])
+    }
+    // return Math.max.apply(null, array)
+}
+
+console.log(verificaIndice([1, 2, 3, 4,]))
