0e727522414b7fc47a8da29e2ca4055918022020
Lara Capila <laracapilak@gmail.com>
2021-03-10 11:05:17 -0300

    exercicio 5 e 6

--- fundamentos/bloco_5/dia_3/script.js
+++ fundamentos/bloco_5/dia_3/script.js
@@ -115,19 +115,40 @@ let dezFridays = [ 4, 11, 18, 25 ];
 mudaTextoSexta(dezFridays);
 
 // Exercício - 6
-let pegaDias = document.querySelectorAll('.day');
-function passaMouse(evt) {
-    // evt.target.style.fontSize = '30px';
-    // evt.target.style.fontWeght = '600';
-    evt.target.style.backgroundColor = 'green'
-    console.log('oiiiiii');
-};
-pegaDias.addEventListener('mouseover', passaMouse);
+// let pegaDias = document.querySelectorAll('.day');
+// function passaMouse(evt) {
+//     // evt.target.style.fontSize = '30px';
+//     // evt.target.style.fontWeght = '600';
+//     evt.target.style.backgroundColor = 'green'
+//     console.log('oiiiiii');
+// };
+// pegaDias.addEventListener('mouseover', passaMouse);
 
-function tiraMouse(evt) {
-    // evt.target.style.fontWeght = '200';
-    // evt.target.style.fontsize = '10px';
-    evt.target.style.backgroundColor = 'yellow'
-    console.log('oiiiiii2');
-};
-pegaDias.addEventListener('mouseout', tiraMouse); 
+// function tiraMouse(evt) {
+//     // evt.target.style.fontWeght = '200';
+//     // evt.target.style.fontsize = '10px';
+//     evt.target.style.backgroundColor = 'yellow'
+//     console.log('oiiiiii2');
+// };
+// pegaDias.addEventListener('mouseout', tiraMouse); 
+
+function dayMouseOver() {
+    let days = document.querySelector('#days');
+  
+    days.addEventListener('mouseover', function(event) {
+      event.target.style.fontSize = '30px';
+      event.target.style.fontWeight = '600';
+    })
+  };
+  
+  function dayMouseOut() {
+    let days = document.querySelector('#days');
+  
+    days.addEventListener('mouseout', function(event) {
+      event.target.style.fontWeight = '200';
+      event.target.style.fontSize = '20px';
+    })
+  };
+  
+  dayMouseOver();
+  dayMouseOut();
