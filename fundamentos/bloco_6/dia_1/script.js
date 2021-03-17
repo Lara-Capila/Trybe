1aae41cc66313b4356b6f06ad2a60fafaa28ea7a
Lara Capila <laracapilak@gmail.com>
2021-03-17 15:00:22 -0300

    botão limpar

--- fundamentos/bloco_6/dia_1/script.js
+++ fundamentos/bloco_6/dia_1/script.js
@@ -16,8 +16,28 @@ function createOptionsStatesBr () {
 function interruptsBtn () {
     let btnSubmit = document.getElementById('send-btn');
 
-    btnSubmit.addEventListener('click', function(event){
-        alert('Formulário enviado com sucesso. Boa sorte!')
+    btnSubmit.addEventListener('submit', function(event){
+        alert('Formulário enviado com sucesso. Boa sorte!');
         event.preventDefault()
     });
+}
+
+function clearDatas () {
+  let inputElements = document.querySelectorAll('input');
+  let texArea = document.querySelector('textarea');
+  let selectElement = document.querySelector('select')
+  console.log(inputElements)
+  
+  for(index = 0; index < inputElements.length; index += 1) {
+    let input = inputElements[index]
+    input.value = "";
+    texArea.value = "";
+    selectElement = "";
+  } 
+}
+
+function eventClearBtn () {
+  let btnClear = document.getElementById('clear-btn');
+
+  btnClear.addEventListener('click', clearDatas);
 }
