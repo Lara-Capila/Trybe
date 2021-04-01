7c1c0535fa101ecb69b50da2c5434b6fa9c94594
Lara Capila <laracapilak@gmail.com>
2021-04-01 18:57:57 -0300

    Inicio parte 2

--- fundamentos/bloco_7/dia_2/objects.js
+++ fundamentos/bloco_7/dia_2/objects.js
@@ -1,3 +1,5 @@
+// PARTE 1
+
 const order = {
   name: 'Rafael Andrade',
   phoneNumber: '11-98763-1416',
@@ -63,3 +65,33 @@ const orderModifier = (order) => {
 }
 
 orderModifier(order);
+
+// PARTE 2
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// 1. Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
+const addShift = (obj, key, value) => {
+  obj[key] = value;
+}
+
+addShift(lesson2, 'Truno', 'manhã' )
