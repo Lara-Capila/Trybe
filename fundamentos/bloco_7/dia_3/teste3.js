3ea98015c26dbbf42e266c23ffc87be50aa862a3
Lara Capila <laracapilak@gmail.com>
2021-04-12 20:17:12 -0300

    implementação teste 3

---
+++ fundamentos/bloco_7/dia_3/teste3.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+assert.strictEqual(typeof myRemoveWithoutCopy, 'function', `Erro: não é uma função!`);
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+const array = [1,2,3,5];
+myRemoveWithoutCopy(array, 4)
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4], `Erro: o item inserido não existe no array`);
